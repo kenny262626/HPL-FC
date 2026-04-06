1cafe2a347d73b01b8c062a6031b8e30c2fcdae1
FORYOUCOM <kenny262626@gmail.com>
2026-04-06 19:50:18 +0900

    layout.tsx import 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,17 @@
-// src/app/layout.tsx
+// app/layout.tsx
 import type { Metadata, Viewport } from "next";
+import "@/styles/hlfc.css";
 
 export const metadata: Metadata = {
-
   description: "Happy Life Football Club",
 };
 
-// 이게 핵심! 모바일 화면 꽉 채우기
 export const viewport: Viewport = {
   width: "device-width",
   initialScale: 1,
   maximumScale: 1,
   userScalable: false,
-  viewportFit: "cover",   // 아이폰 노치/다이나믹 아일랜드까지 꽉 채움
+  viewportFit: "cover",
 };
 
 export default function RootLayout({
